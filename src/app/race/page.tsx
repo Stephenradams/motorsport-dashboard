5190b5a6af34ab5c7458eead375c0c36d7ee78db
Stephen Adams <stephen@cgcsoftware.co.uk>
2024-08-30 15:33:48 +0100

    feat: Add car, driver, and race pages
    This commit adds three new pages to the application: Cars, Drivers, and Race. Each page contains a simple layout with a heading. This is the initial implementation of these pages.

---
+++ src/app/race/page.tsx
@@ -0,0 +1,10 @@
+export default function Race() {
+    return (
+        <main>
+            <div className="flex p-10">
+                <h1 className="text-2xl font-bold">Race</h1>
+
+            </div>
+        </main>
+    );
+}
